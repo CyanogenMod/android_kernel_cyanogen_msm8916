ae925ceca44a234bca6b5dcf51e3e792808c0651
Ethan Chen <intervigil@gmail.com>
2014-10-01 20:31:37 -0700

    msm: mdss: Fix compilation
    Include mdss_mdp_trace header file in trace directory to avoid compile
    errors when compiling standalone kernel image.

--- drivers/video/msm/mdss/mdss_debug.h
+++ drivers/video/msm/mdss/mdss_debug.h
@@ -20,7 +20,7 @@
 #include <linux/mdss_io_util.h>
 
 #include "mdss.h"
-#include "mdss_mdp_trace.h"
+#include <trace/mdss_mdp_trace.h>
 
 #define MISR_POLL_SLEEP		2000
 #define MISR_POLL_TIMEOUT	32000
