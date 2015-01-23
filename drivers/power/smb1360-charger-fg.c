cadce1f1f55f64f261470432f525de045e3b1e27
Scott Mertz <scott@cyngn.com>
2015-01-22 16:08:53 -0800

    power: smb1360: modify with wingtech changes
    Change-Id: I04c332ace5db3105b94554a02c764d5849d3325f

--- drivers/power/smb1360-charger-fg.c
+++ drivers/power/smb1360-charger-fg.c
@@ -1066,6 +1066,13 @@ static int smb1360_get_prop_batt_capacity(struct smb1360_chip *chip)
 	pr_debug("msys_soc_reg=0x%02x, fg_soc=%d batt_full = %d\n", reg,
 						soc, chip->batt_full);
 
+#ifdef CONFIG_MACH_T86519A1
+	if (soc == 100 && chip->batt_full == 0)
+		chip->batt_full = 1;
+	else if (soc < 100 && chip->batt_full == 1)
+		chip->batt_full = 0;
+#endif
+
 	chip->soc_now = (chip->batt_full ? 100 : bound(soc, 0, 100));
 
 	return chip->soc_now;
@@ -4560,6 +4567,10 @@ static int smb1360_resume(struct device *dev)
 	int i, rc;
 	struct i2c_client *client = to_i2c_client(dev);
 	struct smb1360_chip *chip = i2c_get_clientdata(client);
+
+#ifdef CONFIG_MACH_T86519A1
+	power_supply_changed(&chip->batt_psy);
+#endif
 
 	/* Restore the IRQ config */
 	for (i = 0; i < 3; i++) {
