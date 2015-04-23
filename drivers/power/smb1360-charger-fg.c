232ad9a3786568791cee65bc3fb4b6499331395f
jinzhao <jinzhao@wingtech.com>
2015-04-23 17:00:18 +0800

    power: smb1360: Fix battery capacity inaccuracy problem after resume.
    Change-Id: I7915eace3554ee741deafb3a0553875b57b97395

--- drivers/power/smb1360-charger-fg.c
+++ drivers/power/smb1360-charger-fg.c
@@ -4591,10 +4591,6 @@ static int smb1360_resume(struct device *dev)
 	struct i2c_client *client = to_i2c_client(dev);
 	struct smb1360_chip *chip = i2c_get_clientdata(client);
 
-#ifdef CONFIG_MACH_T86519A1
-	power_supply_changed(&chip->batt_psy);
-#endif
-
 	/* Restore the IRQ config */
 	for (i = 0; i < 3; i++) {
 		rc = smb1360_write(chip, IRQ_CFG_REG + i,
@@ -4612,6 +4608,10 @@ static int smb1360_resume(struct device *dev)
 	} else {
 		mutex_unlock(&chip->irq_complete);
 	}
+
+#ifdef CONFIG_MACH_T6519A1
+	power_supply_changed(&chip->batt_psy);
+#endif
 
 	return 0;
 }
