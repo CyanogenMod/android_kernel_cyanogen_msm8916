5f6e52d5ffff41ec96a3134b00be2a1b1298160c
Pat Erley <perley@cyngn.com>
2015-06-02 18:22:06 -0700

    msm_venc: fix parameters for do_div
    do_div() should always take a u64 as it's first parameter.  Not doing
    so was causing msm_venc_s_parm to incorrectly calculate the framerate
    leading to screen record failing.
    
    LETTUCE-564
    Change-Id: I5771aec15a39cabdc33bccdaa88565ebe44b8bbb
    (cherry picked from commit 1049ba5e86e45d75101d5a805a9fbd620bfd5175)

--- drivers/media/platform/msm/vidc/msm_venc.c
+++ drivers/media/platform/msm/vidc/msm_venc.c
@@ -2999,9 +2999,11 @@ int msm_venc_enum_fmt(struct msm_vidc_inst *inst, struct v4l2_fmtdesc *f)
 
 int msm_venc_s_parm(struct msm_vidc_inst *inst, struct v4l2_streamparm *a)
 {
-	u32 property_id = 0, us_per_frame = 0;
+	u32 property_id = 0;
+	u64 us_per_frame = 0;
 	void *pdata;
-	int rc = 0, fps = 0;
+	int rc = 0;
+	u64 fps = 0;
 	struct hal_frame_rate frame_rate;
 	struct hfi_device *hdev;
 
@@ -3046,7 +3048,7 @@ int msm_venc_s_parm(struct msm_vidc_inst *inst, struct v4l2_streamparm *a)
 		fps = fps - 1;
 
 	if (inst->prop.fps != fps) {
-		dprintk(VIDC_PROF, "reported fps changed for %p: %d->%d\n",
+		dprintk(VIDC_PROF, "reported fps changed for %p: %d->%llu\n",
 				inst, inst->prop.fps, fps);
 		inst->prop.fps = fps;
 		frame_rate.frame_rate = inst->prop.fps * (0x1<<16);
