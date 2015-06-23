a89fd453bca2e479bbf9d5897b97a684c1e067b8
Ethan Chen <intervigil@gmail.com>
2015-06-23 12:23:40 -0700

    msm: vidc: Fix FPS calculation for 32-bit
    Change-Id: Ib53de63b5c527bd1a0d66d42104cbeba864a0070

--- drivers/media/platform/msm/vidc/msm_venc.c
+++ drivers/media/platform/msm/vidc/msm_venc.c
@@ -3002,8 +3002,8 @@ int msm_venc_s_parm(struct msm_vidc_inst *inst, struct v4l2_streamparm *a)
 	u32 property_id = 0;
 	u64 us_per_frame = 0;
 	void *pdata;
-	int rc = 0;
-	u64 fps = 0;
+	int rc = 0, fps = 0;
+	u64 base_fps = 0;
 	struct hal_frame_rate frame_rate;
 	struct hfi_device *hdev;
 
@@ -3039,8 +3039,10 @@ int msm_venc_s_parm(struct msm_vidc_inst *inst, struct v4l2_streamparm *a)
 		goto exit;
 	}
 
-	fps = USEC_PER_SEC;
-	do_div(fps, us_per_frame);
+	base_fps = USEC_PER_SEC;
+	do_div(base_fps, us_per_frame);
+
+	fps = (int)base_fps;
 
 	if ((fps % 15 == 14) || (fps % 24 == 23))
 		fps = fps + 1;
@@ -3048,7 +3050,7 @@ int msm_venc_s_parm(struct msm_vidc_inst *inst, struct v4l2_streamparm *a)
 		fps = fps - 1;
 
 	if (inst->prop.fps != fps) {
-		dprintk(VIDC_PROF, "reported fps changed for %p: %d->%llu\n",
+		dprintk(VIDC_PROF, "reported fps changed for %p: %d->%d\n",
 				inst, inst->prop.fps, fps);
 		inst->prop.fps = fps;
 		frame_rate.frame_rate = inst->prop.fps * (0x1<<16);
