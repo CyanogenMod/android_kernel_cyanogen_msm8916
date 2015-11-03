8a80295fef0ae9a7d1a8f747788a515a02be38ff
Khalid Zubair <kzubair@cyngn.com>
2015-11-03 14:56:22 -0800

    input: gsl_ts: replace global lock flag with mutex
    Introduce a mutex to handle synchronization instead of a global lock
    flag.
    
    This patch attempts to maintain the old behavior and gives up if there
    is lock contention instead of delaying.
    
    Change-Id: Ice16e1cef24036f0cbdf9e80dcedf4d21c060212
    Signed-off-by: Khalid Zubair <kzubair@cyngn.com>

--- drivers/input/touchscreen/gsl_ts_driver.c
+++ drivers/input/touchscreen/gsl_ts_driver.c
@@ -46,8 +46,8 @@
     #include <linux/input/mt.h>
 #endif
 
+/** TODO don't need this anymore. ddata->hw_lock gives the same protection */
 static struct mutex gsl_i2c_lock;
-static struct mutex gsl_wake_config_lock;
 
 /* Print Information */
 #ifdef GSL_DEBUG 
@@ -68,7 +68,6 @@ static char int_1st[4] = {0};
 static char int_2nd[4] = {0};
 static char b0_counter = 0;
 static char bc_counter = 0;
-static char i2c_lock_flag = 0;
 #endif
 
 /* Gesture Resume */
@@ -810,10 +809,8 @@ static void gsl_timer_check_func(struct work_struct *work)
 
 	print_info("----------------gsl_monitor_worker------i2c_lock==%d-----------\n",i2c_lock_flag);	
 
-	if(i2c_lock_flag != 0)
+	if(!mutex_trylock(&ddata->hw_lock))
 		goto queue_monitor_work;
-	else
-		i2c_lock_flag = 1;
 
 	gsl_read_interface(gsl_client, 0xb0, read_buf, 4);
     	if(read_buf[3] != 0x5a || read_buf[2] != 0x5a || read_buf[1] != 0x5a || read_buf[0] != 0x5a)
@@ -887,7 +884,7 @@ queue_monitor_init_chip:
 		gsl_sw_init(gsl_client);
 	}
 	
-	i2c_lock_flag = 0;
+	mutex_unlock(&ddata->hw_lock);
 
 queue_monitor_work:	
 	queue_delayed_work(gsl_timer_workqueue, &gsl_timer_check_work,100);
@@ -1490,7 +1487,7 @@ static ssize_t gsl_sysfs_tpgesturet_store(struct device *dev,
 	struct i2c_client *gsl_client = ts->client;
 	int old_gesture_flag;
 
-	mutex_lock(&gsl_wake_config_lock);
+	mutex_lock(&ddata->hw_lock);
 	old_gesture_flag = gsl_gesture_flag;
 	if(buf[0] == '0'){
 		gsl_gesture_flag = 0;  
@@ -1506,7 +1503,7 @@ static ssize_t gsl_sysfs_tpgesturet_store(struct device *dev,
 		if (!old_gesture_flag)
 			enable_irq_wake(gsl_client->irq);
 	}
-	mutex_unlock(&gsl_wake_config_lock);
+	mutex_unlock(&ddata->hw_lock);
 
 	return count;
 }
@@ -1667,10 +1664,8 @@ static irqreturn_t gsl_ts_isr(int irq, void *priv)
 	//if(1 == ddata->gsl_sw_flag)
 	//	goto schedule;
     #ifdef GSL_TIMER
-	if(i2c_lock_flag != 0)
+	if(!mutex_trylock(&ddata->hw_lock))
 		goto i2c_lock_schedule;
-	else
-		i2c_lock_flag = 1;
     #endif
 	
 	#ifdef TPD_PROC_DEBUG
@@ -1834,7 +1829,7 @@ static irqreturn_t gsl_ts_isr(int irq, void *priv)
 	
 schedule:
 #ifdef GSL_TIMER
-	i2c_lock_flag = 0;
+	mutex_unlock(&ddata->hw_lock);
 i2c_lock_schedule:
 #endif	
 	return IRQ_HANDLED;
@@ -2290,7 +2285,7 @@ static int gsl_ts_probe(struct i2c_client *client, const struct i2c_device_id *i
 		goto exit_alloc_cinfo_failed;
 	}
 	mutex_init(&gsl_i2c_lock);
-	mutex_init(&gsl_wake_config_lock);
+	mutex_init(&ddata->hw_lock);
 		
 	ddata->client = client;
 	i2c_set_clientdata(client, ddata);
--- drivers/input/touchscreen/gsl_ts_driver.h
+++ drivers/input/touchscreen/gsl_ts_driver.h
@@ -131,6 +131,8 @@ struct gsl_ts_data{
 	#if 0
 	struct gsl_ts_platform_data *pdata;
 	#endif
+
+	struct mutex hw_lock;
 };
 
 #ifdef GSL_PROXIMITY_SENSOR
