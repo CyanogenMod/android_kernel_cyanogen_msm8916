b1635c4e4d731aee8a724b60cbc23c181db4444f
Khalid Zubair <kzubair@cyngn.com>
2015-11-03 11:09:00 -0800

    ASoC: msm8x16-wcd: remove extra newline from log
    Change-Id: Iefb4041b47d7a990a81bb6e28022d1e6c177dca9

--- sound/soc/codecs/msm8x16-wcd.c
+++ sound/soc/codecs/msm8x16-wcd.c
@@ -2905,7 +2905,7 @@ void wcd_imped_config(struct snd_soc_codec *codec,
 		return;
 	}
 	if (value >= wcd_imped_val[ARRAY_SIZE(wcd_imped_val) - 1]) {
-		pr_err("%s, invalid imped, greater than 48 Ohm\n = %d\n",
+		pr_err("%s, invalid imped, greater than 48 Ohm = %d\n",
 			__func__, value);
 		return;
 	}
