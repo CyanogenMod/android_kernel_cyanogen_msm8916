a0460787ad389a9b6f318421f06febf0dd74eac5
Ricardo Cerqueira <ricardo@cyngn.com>
2015-12-04 12:37:08 +0000

    ASoC: msm: msm8x16: Adjust the detection thresholds for crackling
    Change-Id: I9e523727b1d23a530ac82eefa3f9e0ade9720263

--- sound/soc/msm/msm8x16.c
+++ sound/soc/msm/msm8x16.c
@@ -1374,14 +1374,14 @@ static void *def_msm8x16_wcd_mbhc_cal(void)
 #elif defined(CONFIG_MACH_CRACKLING)
 	btn_low[0] = 75;
 	btn_high[0] = 75;
-	btn_low[1] = 100;
-	btn_high[1] = 100;
-	btn_low[2] = 120;
-	btn_high[2] = 120;
-	btn_low[3] = 350;
-	btn_high[3] = 350;
-	btn_low[4] = 475;
-	btn_high[4] = 475;
+	btn_low[1] = 150;
+	btn_high[1] = 150;
+	btn_low[2] = 237;
+	btn_high[2] = 237;
+	btn_low[3] = 450;
+	btn_high[3] = 450;
+	btn_low[4] = 500;
+	btn_high[4] = 500;
 #else
 	btn_low[0] = 75;
 	btn_high[0] = 75;
