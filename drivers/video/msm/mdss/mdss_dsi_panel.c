fc59c724a2730706883af1653c7fcbb47ad0f380
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-04-26 16:59:07 +0800

    msm: mdss: Remove stray YULONG CE reference
    This is in the wrong place. Either way, it isn't even being used anymore.
    
    Change-Id: I4f53d03afff81f803f19983bded5e6476d2998c2

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -629,10 +629,6 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 	if (ctrl->on_cmds.cmd_cnt)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->on_cmds);
 
-#ifdef CONFIG_YULONG_COLOR
-	color_enhancement_impl_apply();
-#endif
-
 	mdss_livedisplay_update(ctrl, MODE_UPDATE_ALL);
 
 end:
